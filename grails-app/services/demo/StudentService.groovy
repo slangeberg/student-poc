3f99f556b3cf8f3c0c50bcf53b8de935c89c739f
Scott Langeberg <slangeberg@gmail.com>
2014-11-13 21:28:31 -0600

    Got tests working with timeout

---
+++ grails-app/services/demo/StudentService.groovy
@@ -0,0 +1,50 @@
+package demo
+
+class StudentService {
+
+    Student update(long id, int index) {
+
+        Student student = Student.get(id)
+
+        println "StudentService.update() - index: $index, targetId: $id"
+
+        student.name = "conc-$index"
+        student.status = index % 2 == 0 ? 0 : 1
+
+        println "update() { $index, name: ${student.name}, status: ${student.status} }"
+
+        student.save(flush: true, failOnError: true)
+        student
+    }
+
+    Student updateWithLock(long id, int index) {
+
+        Student student = Student.lock(id)
+
+        log.info "StudentService.updateWithLock() - index: $index, targetId: $id"
+
+        student.name = "conc-$index"
+        student.status = index % 2 == 0 ? 0 : 1
+
+        log.info "updateWithLock() { $index, name: ${student.name}, status: ${student.status} }"
+
+        student.save(flush: true, failOnError: true)
+        student
+    }
+
+    Student updateWithTransactionLock(long id, int index) {
+        Student.withTransaction {
+            Student student = Student.lock(id)
+
+            println "StudentService.updateWithTransactionLock() - index: $index, targetId: $id"
+
+            student.name = "conc-$index"
+            student.status = index % 2 == 0 ? 0 : 1
+
+            println "updateWithTransactionLock() { $index, name: ${student.name}, status: ${student.status} }"
+
+            student.save(flush: true, failOnError: true)
+            student
+        }
+    }
+}
